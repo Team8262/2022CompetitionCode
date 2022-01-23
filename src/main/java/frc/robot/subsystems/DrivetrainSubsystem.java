e74d7f92a9da657c3a79a820528820a15d34dbed
= <=>
2022-01-23 11:27:33 -0800

    update jc dependency

--- src/main/java/frc/robot/subsystems/DrivetrainSubsystem.java
+++ src/main/java/frc/robot/subsystems/DrivetrainSubsystem.java
@@ -5,7 +5,7 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import edu.wpi.first.wpilibj.geometry.Translation2d;
+import edu.wpi.first.math.geometry.Translation2d;
 import org.jumprobotics.robot.subsystems.Mk2SwerveDrivetrainFalcon;
 import java.lang.Math;
 import frc.robot.Constants;
