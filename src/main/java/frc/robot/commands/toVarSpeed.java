20e87f33ed4d86ad05bb108e1d895388addbb8d4
nico <nico.bliss.carrascosa@gmail.com>
2022-03-25 22:45:18 -0700

    1 ball auto?? merge

--- src/main/java/frc/robot/commands/toVarSpeed.java
+++ src/main/java/frc/robot/commands/toVarSpeed.java
@@ -30,13 +30,11 @@ public class toVarSpeed extends CommandBase {
   
     // Called once the command ends or is interrupted.
     @Override
-    public void end(boolean interrupted) {
-        m_flywheel.stop();
-    }
+    public void end(boolean interrupted) {}
   
     // Returns true when the command should end.
     @Override
     public boolean isFinished() {
-        return false;
+        return true;
     }
 }
